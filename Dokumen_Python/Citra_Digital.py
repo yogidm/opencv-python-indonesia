ea4e16f6009546c5b6a9290489bc1e5109270f94
mhnd <yogi15@mhs.ee.its.ac.id>
2019-07-17 13:32:24 +0700

    baru lagi

---
+++ Dokumen_Python/Citra_Digital.py
@@ -0,0 +1,18 @@
+import cv2
+
+# Panggil citra digital dengan nama anu.jpeg
+img = cv2.imread("anu.jpeg", 0)
+imb = img.copy()
+imb[imb>100]=255
+imb[imb<100]=0
+
+cv2.imshow ('Gambar', img)
+cv2.imshow ('Gambar BW', imb)
+k = cv2.waitKey(0)
+
+if k == 27:         # menunggu hingga tombol ESC ditekan dan keluar
+    cv2.destroyAllWindows()
+elif k == ord('s'): # menunggu hingga tombol 's' ditekan dan simpan
+    cv2.imwrite('burung.png',img)
+    cv2.imwrite('burungBW.bmp',imb)
+    cv2.destroyAllWindows()
